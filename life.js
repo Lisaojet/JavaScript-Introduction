6d5183e83deef218567f25bee0c892eb049415b2
Elizabeth <105356867+Lisaojet@users.noreply.github.com>
2022-06-30 18:33:45 +0100

    Update life.js

--- life.js
+++ life.js
@@ -3,12 +3,6 @@ let myName = 'My name is Ojetunji Elizabeth';
 let myHeight = '3 meters';
 let myCountry = 'nigeria';
 
-
-let myNameCap = myName.toUpperCase();
-let z = myName.slice(1, 9);
-let firstLetter = myName[0].toUpperCase();
-let name = firstLetter + z 
-
 let myCountryCap = myCountry.toUpperCase();
 let n = myCountry.slice(1, 7);
 let countryLetter = myCountry[0].toUpperCase();
@@ -19,11 +13,3 @@ console.log(myName);
 console.log(myHeight);
 console.log(country); 
 
-/*var name = 'My name is Ojetunji Elizabeth';
-var height = '3 meters';
-var country = 'Nigeria';
-
-
-prompt(name)
-prompt(height)
-prompt(country)*/
